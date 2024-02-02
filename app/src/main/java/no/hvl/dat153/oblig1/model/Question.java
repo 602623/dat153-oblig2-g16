4f93e6f1fb6d76eec75e69682510d5796203c1ce
mnervik <nervik.mathias@gmail.com>
2024-02-02 15:59:13 +0100

    Added Question class

---
+++ app/src/main/java/no/hvl/dat153/oblig1/model/Question.java
@@ -0,0 +1,51 @@
+package no.hvl.dat153.oblig1.model;
+
+import android.graphics.Bitmap;
+
+public class Question {
+    private int imageResId = 0;
+    private Bitmap bitmap = null;
+    private final String answer;
+    private boolean answeredCorrectly = false;
+    private final int id;
+
+    public Question(int image, String name, int resId) {
+        this.imageResId = image;
+        this.answer = name;
+        this.id = resId;
+    }
+
+    public Question(Bitmap bitmap, String name, int id) {
+        this.bitmap = bitmap;
+        this.answer = name;
+        this.id = id;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public boolean isAnsweredCorrectly() {
+        return answeredCorrectly;
+    }
+
+    public void setAnsweredCorrectly(boolean answeredCorrectly) {
+        this.answeredCorrectly = answeredCorrectly;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public boolean isUsingBitmap() {
+        return bitmap != null;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
