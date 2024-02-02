4cbc4d68eb959aafc2b3f3d4e8d788497e62e5fb
mnervik <nervik.mathias@gmail.com>
2024-02-02 16:01:18 +0100

    Updated main activity

---
+++ app/src/main/java/no/hvl/dat153/oblig1/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package no.hvl.dat153.oblig1.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import no.hvl.dat153.oblig1.MyApp;
+import no.hvl.dat153.oblig1.R;
+import no.hvl.dat153.oblig1.model.Question;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyApp app = (MyApp) getApplication();
+        ArrayList<Question> questions = app.getQuestions();
+
+        // Setup the default questions
+        if (questions.isEmpty()) {
+            app.addQuestion(new Question(R.drawable.cat, "cat", app.getLength()));
+            app.addQuestion(new Question(R.drawable.coala, "coala", app.getLength()));
+            app.addQuestion(new Question(R.drawable.dog, "dog", app.getLength()));
+        }
+
+        // Get the components (buttons)
+        Button startQuiz = findViewById(R.id.start_quiz);
+        Button addPhoto = findViewById(R.id.add_photo);
+
+        // Shuffle the questions, so they are in a random order
+        startQuiz.setOnClickListener(v -> {
+            Collections.shuffle(questions);
+            Intent intent = new Intent(this, QuizActivity.class);
+            intent.putExtra("index", 0); // always start at the first random question
+            startActivity(intent);
+        });
+
+        // Redirect to the gallery-activity
+        addPhoto.setOnClickListener(v -> {
+            startActivity(new Intent(this, GalleryActivity.class));
+        });
+    }
+}
