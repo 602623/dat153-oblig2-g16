862e52fab3b2e90cec1319b4c13d24254f5f0a5b
mnervik <nervik.mathias@gmail.com>
2024-02-02 15:59:44 +0100

    Added Gallery Activity

---
+++ app/src/main/java/no/hvl/dat153/oblig1/activity/GalleryActivity.java
@@ -0,0 +1,40 @@
+package no.hvl.dat153.oblig1.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import no.hvl.dat153.oblig1.MyApp;
+import no.hvl.dat153.oblig1.R;
+import no.hvl.dat153.oblig1.adapter.ImageAdapter;
+import no.hvl.dat153.oblig1.model.Question;
+
+public class GalleryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        MyApp app = (MyApp) getApplication();
+        ArrayList<Question> questions = app.getQuestions();
+        questions.sort(Comparator.comparingInt(Question::getId));
+
+        // Get the components
+        Button button = findViewById(R.id.button);
+        GridView gridView = findViewById(R.id.gallery_layout);
+
+        // Use a custom adapter to display the images with text
+        gridView.setAdapter(new ImageAdapter(this, questions));
+
+        button.setOnClickListener(v -> {
+            startActivity(new Intent(this, AddQuestionActivity.class));
+            finish();
+        });
+    }
+}
---
+++ app/src/main/java/no/hvl/dat153/oblig1/adapter/ImageAdapter.java
@@ -0,0 +1,65 @@
+package no.hvl.dat153.oblig1.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import no.hvl.dat153.oblig1.R;
+import no.hvl.dat153.oblig1.model.Question;
+
+public class ImageAdapter extends BaseAdapter {
+    private final Context context;
+    private final ArrayList<Question> questions;
+
+    public ImageAdapter(Context context, ArrayList<Question> questions) {
+        this.context = context;
+        this.questions = questions;
+    }
+
+    @Override
+    public int getCount() {
+        return questions.size();
+    }
+
+    @Override
+    public Object getItem(int index) {
+        return questions.get(index);
+    }
+
+    @Override
+    public long getItemId(int index) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int index, View convertView, ViewGroup parent) {
+        // Create a new view if it doesn't exist, otherwise use the existing view
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.grid_item, parent, false);
+        }
+
+        // Get the image and text views
+        ImageView imageView = view.findViewById(R.id.image);
+        TextView textView = view.findViewById(R.id.text);
+
+        Question question = questions.get(index);
+        if (question.isUsingBitmap()) {
+            // Question is using a bitmap
+            imageView.setImageBitmap(question.getBitmap());
+        } else {
+            // Question is using a resource id
+            imageView.setImageResource(question.getImageResId());
+        }
+        // Set the text view to the answer
+        textView.setText(question.getAnswer());
+
+        return view;
+    }
+}
